330a236329e1e000a9368255906cea56f9b61b12
Bimal dev <bimaldev707@gmail.com>
2024-07-28 12:26:34 +0530

    thrid-commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   
 
   return (
-    <main className='bg-black'>
+    <main className='bg-black '>
     <Navbar/>
     <Hero/> 
     <Highlights/>
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,12 +4,12 @@ import { useGSAP } from '@gsap/react';
 import { create } from 'zustand';
 import { heroVideo, smallHeroVideo } from '../../utils';
 
-
+// Define the Zustand store
 const useStore = create((set) => ({
-  videosrc: window.innerWidth < 720 ? smallHeroVideo : heroVideo,
+  videosrc: window.innerWidth < 760 ? smallHeroVideo : heroVideo,
   setVideosrc: (src) => set({ videosrc: src }),
   handleVideoSrcSet: () => {
-    const newSrc = window.innerWidth < 720 ? smallHeroVideo : heroVideo;
+    const newSrc = window.innerWidth < 760 ? smallHeroVideo : heroVideo;
     set({ videosrc: newSrc });
   },
 }));
@@ -33,25 +33,23 @@ const Hero = () => {
 
   useGSAP(() => {
     gsap.to('#hero', { opacity: 1, delay: 2 });
-    gsap.to('#cta',{ opacity:1, y:-50, delay:2});
+    gsap.to('#cta', { opacity: 1, y: -50, delay: 2 });
   }, []);
 
   return (
-    <section className='w-full nav-height bg-black relative'>
-      <div className='h-5/6 w-full flex-center flex-col'>
-        <p id='hero' className='hero-title'>
-          iPhone 15 Pro
-        </p>
-      </div>
-      <div>
-        <video className='pointer-events-none' autoPlay muted playsInline key={videosrc}>
-          <source src={videosrc} type='video/mp4' />
-        </video>
+    <section className="w-full nav-height bg-black relative">
+      <div className="h-5/6 w-full flex-center flex-col">
+        <p id="hero" className="hero-title">iPhone 15 Pro</p>
+        <div className="md:w-10/12 w-9/12">
+          <video className="pointer-events-none" autoPlay muted playsInline={true} key={videosrc}>
+            <source src={videosrc} type="video/mp4" />
+          </video>
+        </div>
       </div>
 
-      <div id='cta' className='flex flex-col items-center opacity-0  translate-y-20' >
-        <a href="#highlights" className='btn'>Buy</a>
-        <p className='font-normal text-xl'> From ₹5000 month or ₹50,000</p>
+      <div id="cta" className="flex flex-col items-center opacity-0 translate-y-20">
+        <a href="#highlights" className="btn">Buy</a>
+        <p className="font-normal text-xl">From ₹5000 month or ₹50,000</p>
       </div>
     </section>
   );
