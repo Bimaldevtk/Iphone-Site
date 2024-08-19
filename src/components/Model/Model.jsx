c80a6aad0e5a424d7aed9291e2f9eacd84927379
Bimal dev <bimaldev707@gmail.com>
2024-08-19 09:42:07 +0530

    3d view established some fixes are required

--- src/components/Model/Model.jsx
+++ src/components/Model/Model.jsx
@@ -3,8 +3,10 @@ import React, { useRef, useState } from 'react'
 import gsap from 'gsap'
 import ModelView from '../ModelView/ModelView'
 import {yellowImg}  from '../../utils'
+import {models, sizes} from  '../../constants/index'
 import * as THREE from 'three'
 import { Canvas} from '@react-three/fiber';
+import { View } from '@react-three/drei';
 
 
 
@@ -32,47 +34,73 @@ const Model = () => {
     gsap.to('#heading',{y:0,opacity:1})
   },[])
   return (
-    <section className='common-padding'>
-    <div className='screen-max-width'>
+    <section className="common-padding">
+    <div className="screen-max-width">
+      <h1 id="heading" className="section-heading">
+        Take a closer look.
+      </h1>
 
-      <h1  id='heading' className='section-heading'>Take a look Closer...</h1>
-      <div className='flex flex-col items-center mt-5'>
-        <div className='w-full h-[75vh] md:h-[90vh] overflow-hidden relative ' >
-          <ModelView   
-          index={1}
-          groupref={small}
-          gsapType="view1"
-          controlref={cameraControlSmall}
-          setRotationState={setSmallRotation}
-          item={model}
-          size={size}          
-          />
-          <ModelView   
-          index={2}
-          groupref={large}
-          gsapType="view2"
-          controlref={cameraControlLarge}
-          setRotationState={setLargeRotation}
-          item={model}
-          size={size}          
+      <div className="flex flex-col items-center mt-5">
+        <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
+          <ModelView 
+            index={1}
+            groupRef={small}
+            gsapType="view1"
+            controlRef={cameraControlSmall}
+            setRotationState={setSmallRotation}
+            item={model}
+            size={size}
+          />  
+
+          <ModelView 
+            index={2}
+            groupRef={large}
+            gsapType="view2"
+            controlRef={cameraControlLarge}
+            setRotationState={setLargeRotation}
+            item={model}
+            size={size}
           />
 
-         <Canvas className="h-full w-full"
-         style={{overflow:'hidden', top:0 ,bottom:0, left:0, right:0}}
-         eventSource={document.getElementById('root')}
-         >
-          {/* <View.Port/> */}
-         </Canvas>
+          <Canvas
+            className="w-full h-full"
+            style={{
+              position: 'fixed',
+              top: 0,
+              bottom: 0,
+              left: 0,
+              right: 0,
+              overflow: 'hidden'
+            }}
+            eventSource={document.getElementById('root')}
+          >
+            <View.Port />
+          </Canvas>
         </div>
-        <div className='mx-auto h-full'>
-          <p>{model.title}</p>
+
+        <div className="mx-auto w-full">
+          <p className="text-sm font-light text-center mb-5">{model.title}</p>
+
+          <div className="flex-center">
+            <ul className="color-container">
+              {models.map((item, i) => (
+                <li key={i} className="w-6 h-6 rounded-full mx-2 cursor-pointer" style={{ backgroundColor: item.color[0] }} onClick={() => setModel(item)} />
+              ))}
+            </ul>
+
+            <button className="size-btn-container">
+              {sizes.map(({ label, value }) => (
+                <span key={label} className="size-btn" style={{ backgroundColor: size === value ? 'white' : 'transparent', color: size === value ? 'black' : 'white'}} onClick={() => setSize(value)}>
+                  {label}
+                </span>
+              ))}
+            </button>
+          </div>
         </div>
       </div>
- 
-      
     </div>
-    </section>
-  )
+  </section>
+)
 }
 
 export default Model
--- src/components/ModelView/ModelView.jsx
+++ src/components/ModelView/ModelView.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { Suspense } from 'react';
+import { Html, OrbitControls, PerspectiveCamera, View } from '@react-three/drei';
+import Lights from '../Lights/Lights';
+import Iphone from '../Iphone/Iphone';
+import * as THREE from 'three';
+import Loader from '../Loader/Loader';
 
-const ModelView = () => {
+const ModelView = ({ index, groupref, gsapType, controlref, setRotationState, item, size }) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <View
+      index={index}
+      id={gsapType}
+      className={` w-full h-full ${index === 2 ? 'right-[-100]':''}`}
+    >
+      <ambientLight intensity={0.3} />
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+      <Lights />
 
-export default ModelView
+      <OrbitControls
+        makeDefault
+        ref={controlref}
+        enableZoom={false}
+        enablePan={false}
+        rotateSpeed={0.4}
+        target={new THREE.Vector3(0, 0, 0)}
+        onEnd={() => setRotationState(controlref.current.getAzimuthAngle())}
+      />
+
+      <group ref={groupref}>
+        <Suspense fallback={<Loader/> }>
+          <Iphone scale={index === 1 ? [15, 15, 15] : [17, 17, 17]} 
+          item={item}
+          size={size}
+          />
+        </Suspense>
+      </group>
+    </View>
+  );
+};
+
+export default ModelView;
